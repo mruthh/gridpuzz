4a41eb39cd3f442147fdd6f71849d1bffacbfce8
Megan <meganhonig@gmail.com>
2022-02-16 13:22:54 -0500

    allow selecting groups and validating selections

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="grid">
-    <template v-for="square in grid" :key="square.item">
-      <button class="grid-item">{{ square.item }}</button>
+    <template v-for="(square, index) in grid" :key="square.item">
+      <button 
+        class="grid-item"
+        :style="square.selected ? 'border: 2px solid red' : null"
+        @click="toggleSelected(index)"
+      >
+          {{ square.item }}
+        </button>
     </template>  
   </div>
 </template>
 
 <script>
 
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
-const group1 = { items: ['Bea', 'Mae', 'Angus', 'Selmers'], connections: { description: 'Night in the woods' } }
-const group2 = { items: ['Gregg', 'Robert\'s', 'Road', 'House'], connections: { description: 'Rules' } }
-const group3 = { items: ['Germ', 'Paste', 'Grass', 'Bran'], connections: { description: 'Wheat' } }
-const group4 = { items: ['Ground', 'Calm', 'Settle', 'Center'], connections: { description: 'Find peace' } }
+const group1 = { id: 1, items: ['Bea', 'Mae', 'Angus', 'Selmers'], connections: { description: 'Night in the woods' } }
+const group2 = { id: 2, items: ['Gregg', 'Robert\'s', 'Road', 'House'], connections: { description: 'Rules' } }
+const group3 = { id: 3, items: ['Germ', 'Paste', 'Grass', 'Bran'], connections: { description: 'Wheat' } }
+const group4 = { id: 4, items: ['Ground', 'Calm', 'Settle', 'Center'], connections: { description: 'Find peace' } }
 
 
 export default {
@@ -30,7 +36,7 @@ export default {
       const item = group.items.splice(itemIndex, 1)[0]
       
       if (!group.items.length) groups.splice(groupIndex, 1)
-      grid.push({ item, connections: group.connections })
+      grid.push({ item, groupId: group.id, selected: false })
 
       return groups.length ? buildGrid(groups, grid) : grid
     }
@@ -38,7 +44,36 @@ export default {
 
     const grid = ref(buildGrid([group1, group2, group3, group4]))
 
-    return { grid }
+    const selectedItems = computed(() => {
+      return grid.value.filter(item => item.selected)
+    })
+
+    const resetItems = () => {
+      for (const item of grid.value) {
+        item.selected = false
+      }
+    }
+
+    const isValidGroup = items => {
+      return items.every(item => item.groupId === items[0].groupId)
+    }
+
+    const validateGroup = items => {
+      if (isValidGroup(items)) {
+        alert('You found a group!')
+      }
+      else alert('Sorry, not a group')
+
+      resetItems()
+    }
+
+    const toggleSelected = index => {
+      grid.value[index].selected = !grid.value[index].selected
+
+      if (selectedItems.value.length >= 4) validateGroup(selectedItems.value)
+    }
+
+    return { grid, toggleSelected }
   }
 }
 </script>
