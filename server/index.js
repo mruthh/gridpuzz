8c82f992ebd2c50e07596a7f84c5544848ac4ada
Megan <meganhonig@gmail.com>
2022-02-19 15:19:42 -0500

    chore: add link to comment for reference

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const { Server } = require('node-static')
 const server = new Server('./dist')
 
 // copied wholesale from the Node Static README :)
+// https://github.com/cloudhead/node-static
 require('http').createServer(function (request, response) {
   request.addListener('end', function () {
     //
