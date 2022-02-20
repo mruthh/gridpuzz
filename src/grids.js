4e874a310f0158a98befde6671d562e8e2b8ac3d
Megan <meganhonig@gmail.com>
2022-02-20 15:35:13 -0500

    feat: add new grid

--- src/grids.js
+++ src/grids.js
@@ -62,8 +62,40 @@ const group8 = {
   } 
 }
 
+const group9 = { 
+  id: 1, 
+  items: ['Stoat', 'Price', 'Misspelt', 'Acorn'], 
+  connections: { 
+    description: 'They end with grains (oat, rice, spelt, corn)',
+    keywords: ['grain']
+  } }
+const group10 = { 
+  id: 2, 
+  items: ['Bap', 'Pippped', 'Chuffed', "Lorry"], 
+  connections: {
+    description: 'Things the British say',
+    keywords: ['British', 'English', 'UK']
+  } 
+}
+const group11 = { 
+  id: 3, 
+  items: ['Elk', 'Mantis', 'Bee', 'Sparrow'], 
+  connections: { 
+    description: 'Cards in Inscryption',
+    keywords: ['Inscryption', 'incscription', 'cards']
+  } 
+}
+const group12 = { 
+  id: 4, 
+  items: ['Yeet', 'Scam', 'Grant', 'Horsey'], 
+  connections: { 
+    description: 'NPCs from Dungeons and Daddies',
+    keywords: ['dungeons', 'daddies', 'characters', 'npc']
+  } 
+}
 
 export const grids = [
   [group1, group2, group3, group4],
-  [group5, group6, group7, group8]
+  [group5, group6, group7, group8],
+  [group9, group10, group11, group12]
 ]
